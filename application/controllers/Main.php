a9c2a59b11d2692bb9a8b5023f932c091eda5ed4
kimildo <kimildo78@gmail.com>
2017-02-10 11:41:55 +0900

    마스터 메소드 정리

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -31,44 +31,17 @@
 
         }
 		
+        /**
+         * @return array
+         * 오류사항 수정
+         */
 		public function masterMethod()
         {
 			$master = array();
 			return $master;
 		}
 
-        /**
-         * @return array
-         * 오류사항 수정
-         */
-		public function masterAddMethod()
-        {
-            $master = array();
-            return $master;
-        }
         
-        public function masterAddMethod2()
-        {
-        	$master = array();
-        	return $master;
-        }
         
-        public function masterAddMethod3()
-        {
-        	$master = array();
-        	return $master;
-        }
-        
-        public function masterAddMethod4()
-        {
-        	$master = array();
-        	return $master;
-        }
-        
-        public function masterAddMethod5()
-        {
-        	$master = array();
-        	return $master;
-        }
 
     }
