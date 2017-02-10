67d0e7b9ea0a2c0ac3d98fab1b1059d1f3c2d0fb
kimildo <kimildo78@gmail.com>
2017-02-10 13:05:56 +0900

    마스터 초기화 - master

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -18,6 +18,50 @@
             }
         }
 
+        public function masterMethod()
+        {
+            $master = array();
+            return $master;
+        }
+
+        /**
+         * @return array
+         * 오류사항 수정
+         */
+        public function masterAddMethod()
+        {
+            $master = array();
+            return $master;
+        }
+
+        /**
+         * @return array
+         * dfefe
+         */
+        public function masterAddMethod2()
+        {
+            $master = array();
+            return $master;
+        }
+
+        public function masterAddMethod3()
+        {
+            $master = array();
+            return $master;
+        }
+
+        public function masterAddMethod4()
+        {
+            $master = array();
+            return $master;
+        }
+
+        public function masterAddMethod5()
+        {
+            $master = array();
+            return $master;
+        }
+
         public function index()
         {
             $debugbar = new StandardDebugBar();
